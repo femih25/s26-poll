08a5541d1b419460e94496f827cd6f8cabc116b9
femih25 <femihorrall2027@u.northwestern.edu>
2026-05-05 20:41:26 -0500

    test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@
 function Home() {
     return(
     <div id = "home"> 
-      <h1>The Daily Northwestern's! <br /> Spring 2026 Poll</h1>
+      <h1>The Daily Northwestern's <br /> Spring 2026 Poll</h1>
     </div> 
   )
   }
