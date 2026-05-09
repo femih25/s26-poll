b36cbfde20a9294dbbd240435e6273b08b78baaf
aboyko22 <alexboyko2026@u.northwestern.edu>
2026-05-09 14:00:45 -0500

    placeholders and style updates

--- src/components/category-icons.jsx
+++ src/components/category-icons.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 
 const categories = [
-    { name: 'Well-Being', path: '/wellbeing', icon: '/public/placeholder.png' },
-    { name: 'AI', path: '/ai', icon: '/public/placeholder.png' },
-    { name: 'Sports', path: '/sports', icon: '/public/placeholder.png' },
-    { name: 'Jobs', path: '/jobs', icon: '/public/placeholder.png' },
-    { name: 'NU Issues', path: '/nu-issues', icon: '/public/placeholder.png' },
-    { name: 'National Politics', path: '/national-politics', icon: '/public/placeholder.png' },
+    { name: 'Well-Being', path: '/wellbeing', icon: 'https://img.icons8.com/?size=2000&id=19166&format=png&color=000000', color: "#a0a972"},
+    { name: 'AI', path: '/ai', icon: 'https://img.icons8.com/?size=2000&id=R13upTV7Q1bg&format=png&color=000000', color: "#bedce7"},
+    { name: 'Sports', path: '/sports', icon: 'https://img.icons8.com/?size=2000&id=9817&format=png&color=000000', color: "#f0a94f"},
+    { name: 'Jobs', path: '/jobs', icon: 'https://img.icons8.com/?size=2000&id=2784&format=png&color=000000', color: "#4b7b74"},
+    { name: 'NU Issues', path: '/nu-issues', icon: 'https://img.icons8.com/?size=2000&id=42179&format=png&color=000000', color: "#705294"},
+    { name: 'National Politics', path: '/national-politics', icon: 'https://img.icons8.com/?size=2000&id=4682&format=png&color=000000', color: "#f1d279"},
   ]
 
 
@@ -16,7 +16,7 @@ function CategoryIcons() {
     return(
         <div className="category-icon-grid">
             {categories.map((cat) => (
-                <Link to={cat.path} key={cat.name} className="category-icon">
+                <Link to={cat.path} key={cat.name} className="category-icon" style={{ '--hover-color': cat.color }}>
                     <img src={cat.icon} alt={cat.name} />
                     <span>{cat.name}</span>
                 </Link>
